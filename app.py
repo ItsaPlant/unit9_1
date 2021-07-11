acdd3ae6e9846ba9f526f04e1f51d13fdfd897b8
ItsaPlant <krzysiekmichalik6@gmail.com>
2021-07-11 19:23:27 +0200

    does not work, don't know why

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask
 from flask import request, redirect
 from flask import render_template
 
+data = {}
+with open("bank_quest_simple.csv", newline='') as csvfile:
+    csvreader = csv.reder(csvfile, delimiter=';', quotechar='|')
+    for row in csvreader:
+        data[row[1]]=row[2]
+
 app = Flask(__name__)
 
 @app.route("/currency/calc", methods=["GET", "POST"])
@@ -11,4 +17,12 @@ def calculator():
         return render_template("calc.html")
     elif request.method=="POST":
         cur_1=request.form.get("cur_1")
-        cur_2=request.form.get("cur_2")
+        quant=request.form.get('quant')
+
+        #cur_1=data.get(cur_1)
+
+        cost = cur_1 * quant
+
+        return render_template("calc_result.html", cost=cost)
+        
+
